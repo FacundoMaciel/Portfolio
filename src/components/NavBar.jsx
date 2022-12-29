b761705d5da2ecc259f61470a8551a48a14c50a6
lvlaciel <96140694+lvlaciel@users.noreply.github.com>
2022-12-29 15:55:42 -0300

    NewCV

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -87,7 +87,7 @@ const NavBar = () => {
             </li>
           ))}
           <li className="px-4 cursor-pointer capitalize py-6 text-4xl">
-            <a href="/CV-FacundoMacielfoto.pdf" download>
+            <a href="/CV-FacundoMacielDeveloper.pdf" download>
               <button>CV</button>
             </a>
           </li>
