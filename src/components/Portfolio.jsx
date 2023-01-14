e2b5fb29716d87a601bdabf2d2999c6657f65a3e
lvlaciel <96140694+lvlaciel@users.noreply.github.com>
2023-01-13 21:55:14 -0300

    AddProyectGreydive

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import RGBTech from "../assets/RGBTech.png";
 import pifoods from "../assets/newFoods.png";
-import weatherNative from "../assets/weatherNative.png"
 import BsaleShop from "../assets/Bsale.png"
 import SabumFit from "../assets/SabumFit.png"
+import Greydive from "../assets/Greydive.png"
 
 
 const Portfolio = () => {
@@ -12,31 +12,36 @@ const Portfolio = () => {
       id: 1,
       src: RGBTech,
       href: "https://rgbtech.vercel.app/",
-      gitHub: "https://github.com/RGBTech-PF/rgbtech",   
+      gitHub: "https://github.com/RGBTech-PF/rgbtech",
+      name: "RGBTech E-commerce tecnológico"   
     },
     {
       id: 2,
       src: pifoods,
       href: "https://pi-food-two.vercel.app/",
-      gitHub: "https://github.com/lvlaciel/Pi-Food", 
-    },
-    {
-      id: 3,
-      src: weatherNative,
-      href: "https://www.linkedin.com/feed/update/urn:li:activity:6987775245680062464/",
-      gitHub: "https://github.com/lvlaciel/weatherApp", 
+      gitHub: "https://github.com/lvlaciel/Pi-Food",
+      name: "Proyecto individual foods-app" 
     },
     {
       id: 4,
       src: BsaleShop,
       href: "https://challenge-bsale-frontend.vercel.app/",
-      gitHub: "https://github.com/lvlaciel/challengeBsale-frontend", 
+      gitHub: "https://github.com/lvlaciel/challengeBsale-frontend",
+      name: "BsaleShop tienda online" 
     },
     {
       id: 5,
       src: SabumFit,
       href: "https://gym-sabum-fit.vercel.app/",
-      gitHub: "https://github.com/lvlaciel/gym_sabumFit", 
+      gitHub: "https://github.com/lvlaciel/gym_sabumFit",
+      name: "SabumFit Gym" 
+    },
+    {
+      id: 6,
+      src: Greydive,
+      href: "https://challenge-greydive-alpha.vercel.app/",
+      gitHub: "https://github.com/lvlaciel/challenge-Greydive",
+      name: "Greydive Form" 
     },
   ];
 
@@ -54,8 +59,9 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-1 mr-2 md:grid-cols-3 gap-8 px-2 pb-5 sm:px-0">
-          {portfolios.map(({ id, src, href, gitHub }) => (
-            <div key={id} className="shadow-lg shadow-gray-600 rounded-lg dark:text-white">
+          {portfolios.map(({ id, src, href, gitHub, name }) => (
+            <div key={id} className="shadow-lg shadow-gray-600 rounded-lg dark:text-white text-center">
+              <h2 className="font-bold mb-1">{name}</h2>
               <img
                 src={src}
                 alt="Not found"
@@ -67,7 +73,7 @@ const Portfolio = () => {
                 >
                 <button className="w-1/2 px-6 py-2 m-4 mr-1 duration-150 hover:scale-105 font-semibold hover:text-white dark:hover:text-cyan-300"
                 >
-                Ver
+                Deploy
                 </button>
                 </a>
                 <a href={gitHub}
